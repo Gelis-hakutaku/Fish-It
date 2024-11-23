a00ce59d4cd091b1ea51d9d1f51e63f0e6e739d3
Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
2024-11-23 22:16:04 +0100

    Update Ammo_Behaviour.cs

--- Assets/Scripts/Ammo_Behaviour.cs
+++ Assets/Scripts/Ammo_Behaviour.cs
@@ -7,15 +7,15 @@ public class Ammo_Behaviour : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private ScoreManager _score;
+    [SerializeField] private TimerManager _timer;
+    [SerializeField] private int bonusChain;
 
     private Transform target;
-    private int bonusChain;
+
 
 
     private void Start()
     {
-        bonusChain = 1;
-
         target = FindObjectOfType<Follow_Cursor>().transform;
 
         StartCoroutine(LifeTime());
@@ -34,12 +34,30 @@ public class Ammo_Behaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
-        CompareTag("Fish");
-            {
-                _score.score += 10 * bonusChain;
-                bonusChain *= 2;
-                Destroy(collision.gameObject);
-            }
+        if (collision.CompareTag("Fish"))
+        {
+            _score.score += 10 * bonusChain;
+            bonusChain *= 2;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("RainbowFish"))
+        {
+            _timer.duration += 10;
+            _score.score += 10 * bonusChain;
+            bonusChain *= 2;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Squid"))
+        {
+
+        }
+
+        if (collision.CompareTag("PufferFish"))
+        {
+
+        }
         Debug.Log(_score.score);
     }
 }
